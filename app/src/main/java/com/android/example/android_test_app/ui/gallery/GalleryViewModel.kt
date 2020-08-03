0677f469c345563870df2d34eb02ef9635acfa13
Roman Huster <rh@fluidmobile.de>
2020-08-03 10:54:41 +0200

    RecyclerView doesn't show anything

--- app/src/main/java/com/android/example/android_test_app/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/android/example/android_test_app/ui/gallery/GalleryViewModel.kt
@@ -1,13 +1,27 @@
 package com.android.example.android_test_app.ui.gallery
 
+import android.Manifest
+import android.app.Application
+import android.content.Context
+import android.database.Cursor
+import android.provider.MediaStore
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.RuntimePermissions
 
-class GalleryViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+class GalleryViewModel(val context: Application) : AndroidViewModel(context) {
+
+    private val _text = MutableLiveData<List<String>>()
+
+    val text: LiveData<List<String>> = _text
+
+
+    fun setAllImagePaths(paths: List<String>){
+        _text.value = paths
     }
-    val text: LiveData<String> = _text
 }
